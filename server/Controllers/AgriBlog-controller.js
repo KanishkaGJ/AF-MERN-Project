65c42f1eb54758e69dc4ffdb432a3ee999ff6616
IT21016684 <110347124+KanishkaGJ@users.noreply.github.com>
2024-09-25 12:03:31 +0530

    error handling

--- server/Controllers/AgriBlog-controller.js
+++ server/Controllers/AgriBlog-controller.js
@@ -11,25 +11,31 @@ const addAgriBlog = async (req, res, next) => {
   });
 
   newAgriBlog
-    .save()
-    .then(() => {
-      res.json("Agri Blog Added!!");
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+  .save()
+  .then(() => {
+    res.status(201).json({ message: "Agri Blog Added!" }); // Use status 201 for created
+  })
+  .catch((err) => {
+    console.error("Error adding Agriculture blog:", err); // Log detailed error for debugging
+    res.status(500).json({ message: "Failed to add Agriculture blog. Please try again later." }); // Generic message for client
+  });
 };
 
 //read all agri blogs
 const getAllAgriBlogs = async (req, res, next) => {
   AgriBlog.find()
     .then((agriblogs) => {
-      res.json(agriblogs);
+      if (!agriblogs || agriblogs.length === 0) {
+        return res.status(404).json({ message: "No Agriculture Blogs found." });
+      }
+      res.status(200).json(agriblogs); // Success response
     })
     .catch((err) => {
-      console.log(err);
+      console.error("Error retrieving Agriculture blogs:", err); // Log detailed error
+      res.status(500).json({ message: "Unable to retrieve blogs. Please try again later." }); // Generic message for client
     });
 };
+
 
 //get agri blog by id
 exports.getAgriById = async (req, res) => {
@@ -41,7 +47,7 @@ exports.getAgriById = async (req, res) => {
     })
     .catch(() => {
       console.log(err.message);
-      res.status(500).send({ status: "error", error: err.message });
+      res.status(500).send({ status: "Could not fetch Blog", error: err.message });
     });
 };
 
